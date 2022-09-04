0bc3e48c1028a29471893878380b28307a20d89d
iamalmiir <78283120+iamalmiir@users.noreply.github.com>
2022-09-04 05:48:19 -0400

    Update profanity_filters.py

--- users/helpers/profanity_filters.py
+++ users/helpers/profanity_filters.py
@@ -5,6 +5,6 @@ profanity.load_censor_words_from_file("users/helpers/profanity_words.txt")
 
 def register_data_filter(data):
     credentials = [data["full_name"], data["email"], data["username"]]
-    for item in data_to_filer:
-        if profanity.contains_profanity(item):
+    for credential in credentials:
+        if profanity.contains_profanity(credential):
             raise ValueError("Profanity is not allowed.")
