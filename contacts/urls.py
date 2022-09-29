ed8e82d90b7278506fc7123828cab78ffaf9442a
iamalmiir <iamalmiir@gmail.com>
2022-09-29 19:45:05 -0400

    code improvements

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,16 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from contacts import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("realtors.urls")),
-    path("api/", include("listings.urls")),
-    path("api/", include("contacts.urls")),
-    path("api/user/", include("users.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("contact/business-inquiries/", views.CreateBusinessInquiry.as_view()),
+    path("inquiries/", views.CreateInquiry.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
