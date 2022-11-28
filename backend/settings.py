28d36ea3bdea37ee833ab095107a671fe485e2be
iamalmiir <iamalmiir@gmail.com>
2022-11-27 20:57:54 -0500

    small changes and bug fixes

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type UUID
 DEFAULT_AUTO_FIELD = "django.db.models.UUIDField"
