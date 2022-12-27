efe0c4da636f3bf75ff920b3482d239d1c973ae4
iamalmiir <iamalmiir@gmail.com>
2022-12-27 18:17:41 -0500

    now it can be seen if realtor is mvp

--- realtors/serializers.py
+++ realtors/serializers.py
@@ -6,4 +6,4 @@ from realtors.models import Realtor
 class RealtorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Realtor
-        fields = ["slug", "full_name", "photo", "description", "phone", "email"]
+        fields = ["slug", "full_name", "photo", "description", "phone", "email", "is_mvp"]
