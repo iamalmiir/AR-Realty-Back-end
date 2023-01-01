e09df7911308825fd616be9ac86003d98cf8ee1b
iamalmiir <iamalmiir@gmail.com>
2023-01-01 16:00:19 -0500

    small bug fixes

--- realtors/models.py
+++ realtors/models.py
@@ -1,7 +1,6 @@
 import datetime
 from uuid import uuid4
 
-import cloudinary.uploader
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -11,7 +10,7 @@ class Realtor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     full_name = models.CharField(max_length=100)
     slug = models.SlugField(editable=False, unique=True, max_length=50, null=True, blank=True)
-    photo = models.ImageField(upload_to="realtors/%Y/%m/%d/", blank=True)
+    photo = models.ImageField(upload_to="ar/realtors/%Y/%m/%d/", blank=True)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=50)
